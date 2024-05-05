4ecd9a9e5376c9d1f4ddfd9c3e0f1413f49d9197
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-05-05 16:55:28 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter fristNumber", type: "number", name: "fristNumber" },
+    { message: "enter secondNumber", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division",]
+    }
+]);
+if (answer.operator === "Addition") {
+    let result = answer.fristNumber + answer.secondNumber;
+    console.log(result);
+}
+else if (answer.operator === "Subtraction") {
+    let result = answer.fristNumber - answer.secondNumber;
+    console.log(result);
+}
+else if (answer.operator === "Multiplication") {
+    let result = answer.fristNumber * answer.secondNumber;
+    console.log(result);
+}
+else if (answer.operator === "Division") {
+    let result = answer.fristNumber / answer.secondNumber;
+    console.log(result);
+}
+;
